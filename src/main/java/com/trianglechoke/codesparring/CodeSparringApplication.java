df8a7290ab7bc65850863d945aa9b7b211d68b73
leejaeseong11 <easeon78@gmail.com>
2023-11-17 17:14:16 +0900

    init repo

---
+++ src/main/java/com/trianglechoke/codesparring/CodeSparringApplication.java
@@ -0,0 +1,12 @@
+package com.trianglechoke.codesparring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeSparringApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CodeSparringApplication.class, args);
+  }
+}
---
+++ src/test/java/com/trianglechoke/codesparring/CodesparringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trianglechoke.codesparring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodesparringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
