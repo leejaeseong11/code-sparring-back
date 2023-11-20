962869baf51c265884046e9b984ed055f9c28838
leejaeseong11 <easeon78@gmail.com>
2023-11-20 15:15:57 +0900

    chore: add config file

---
+++ src/main/java/com/trianglechoke/codesparring/config/MyMVCContext.java
@@ -0,0 +1,7 @@
+package com.trianglechoke.codesparring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyMVCContext implements WebMvcConfigurer {}
