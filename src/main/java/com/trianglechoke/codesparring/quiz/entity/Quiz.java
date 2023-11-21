51f9ef44be0eacada19a4f5405749f402b860e26
qbobl5 <ddddd991125@gmail.com>
2023-11-21 16:09:19 +0900

    Fix entity
    정렬 + 오타 수정

--- src/main/java/com/trianglechoke/codesparring/quiz/entity/Quiz.java
+++ src/main/java/com/trianglechoke/codesparring/quiz/entity/Quiz.java
@@ -1,7 +1,5 @@
 package com.trianglechoke.codesparring.quiz.entity;
 
-
-
 import java.util.ArrayList;
 import java.util.List;
 import org.hibernate.annotations.DynamicInsert;
@@ -23,63 +21,57 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-@Getter @NoArgsConstructor @AllArgsConstructor @ToString
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 @Builder
 @Entity
-@Table(name="Quiz")
+@Table(name = "Quiz")
 @DynamicInsert
-
-@SequenceGenerator(name = "quiz_seq_generator",
-sequenceName = "quiz_seq",
-
-
-initialValue = 1000, //시작 숫자
-allocationSize = 1) //증가하는 숫자
+@SequenceGenerator(
+        name = "quiz_seq_generator",
+        sequenceName = "quiz_seq",
+        initialValue = 1000, // 시작 숫자
+        allocationSize = 1) // 증가하는 숫자
 public class Quiz {
-  @Id
-  @Column(name="quize_no", nullable = false)
-  @GeneratedValue(strategy = GenerationType.SEQUENCE,
-  generator = "quize_seq_generator")
+    @Id
+    @Column(name = "quiz_no", nullable = false)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "quiz_seq_generator")
+    private Long quizNo;
 
-  private Long quizNo;
+    // 회원번호 FK(Member)
+    @ManyToOne
+    @JoinColumn(name = "member_no")
+    private Member member;
 
+    @Column(name = "quiz_title", nullable = false, length = 60)
+    private String quizTitle;
 
+    @Column(name = "quiz_content", nullable = false, length = 30000)
+    private String quizContent;
 
-  //회원번호 FK(Member)
-  @ManyToOne
-  @JoinColumn(name = "member_no")
-  private Member member;
+    @Column(name = "quiz_tier", length = 8)
+    private String quizTier;
 
-  @Column(name="quiz_title", nullable = false, length = 60)
-  private String quizTitle;
+    @Column(name = "quiz_status", nullable = false, length = 1)
+    private Integer quizStatus;
 
-  @Column(name="quiz_content", nullable = false, length = 30000)
-  private String quizContent;
+    @Column(name = "testcase_cnt", nullable = false)
+    private Integer testcaseCnt;
 
-  @Column(name="quiz_tier", length = 8)
-  private String quizTier;
+    @Column(name = "quiz_submit_cnt", nullable = false)
+    private Integer quizSubmitCnt;
 
-  @Column(name="quiz_status", nullable = false, length = 1)
-  private Integer quizStatus;
+    @Column(name = "quiz_success_cnt", nullable = false)
+    private Integer quizSuccessCnt;
 
-  @Column(name="testcase_cnt", nullable = false)
-  private Integer testcaseCnt;
+    @Column(name = "quiz_input", length = 500)
+    private String quizInput;
 
+    @Column(name = "quiz_output", length = 500)
+    private String quizOutput;
 
-
-  @Column(name="quiz_submit_cnt", nullable = false)
-  private Integer quizSubmitCnt;
-
-  @Column(name="quiz_success_cnt", nullable = false)
-  private Integer quizSuccessCnt;
-
-  @Column(name="quiz_input", length = 500)
-  private String quizInput;
-
-  @Column(name="quiz_output", length = 500)
-  private String quizOutput;
-
-
-  @OneToMany(mappedBy = "board")
-  List<Report> posts = new ArrayList<>();
+    @OneToMany(mappedBy = "board")
+    List<Report> posts = new ArrayList<>();
 }
