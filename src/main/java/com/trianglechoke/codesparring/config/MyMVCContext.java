13ed40be592d20dcb09ebed48e7a19dc9d163e6e
leejaeseong11 <easeon78@gmail.com>
2023-11-21 12:27:27 +0900

    feat: set front url for cors

--- src/main/java/com/trianglechoke/codesparring/config/MyMVCContext.java
+++ src/main/java/com/trianglechoke/codesparring/config/MyMVCContext.java
@@ -1,18 +1,24 @@
 package com.trianglechoke.codesparring.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
+@PropertySource("classpath:/application.properties")
 public class MyMVCContext implements WebMvcConfigurer {
+    @Value("${app.frontURL}")
+    private String frontURL;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedOriginPatterns("http://192.168.1.83:5500");
+                .allowedOriginPatterns(frontURL);
     }
 }
