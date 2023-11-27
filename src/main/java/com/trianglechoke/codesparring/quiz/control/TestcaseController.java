f791a1782eafdde9b3de33d396be22c20b797f5a
qbobl5 <ddddd991125@gmail.com>
2023-11-27 18:03:30 +0900

    fix testcase save
    testcase(+testcaseInput) 추가 기능 수정

--- src/main/java/com/trianglechoke/codesparring/quiz/control/TestcaseController.java
+++ src/main/java/com/trianglechoke/codesparring/quiz/control/TestcaseController.java
@@ -1,8 +1,13 @@
 package com.trianglechoke.codesparring.quiz.control;
 
+import com.trianglechoke.codesparring.exception.ErrorCode;
+import com.trianglechoke.codesparring.exception.MyException;
+import com.trianglechoke.codesparring.quiz.dto.TestcaseDTO;
 import com.trianglechoke.codesparring.quiz.service.TestcaseService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,19 +22,19 @@ public class TestcaseController {
     //    }
 
     /* 문제에 테스트케이스 추가하기 : 관리자 */
-    //    @PostMapping("/{quizNo}")
-    //    public ResponseEntity<?> writeTestcase(
-    //            @PathVariable Long quizNo, @RequestBody TestcaseDTO testcaseDTO) throws
-    // AddException {
-    //        testcaseDTO.setQuizNo(quizNo);
-    //        try {
-    //            service.addTestcase(testcaseDTO);
-    //            String msg = "테스트케이스 추가 성공";
-    //            return new ResponseEntity<>(msg, HttpStatus.OK);
-    //        } catch (AddException e) {
-    //            throw new AddException("테스트케이스 추가 실패");
-    //        }
-    //    }
+        @PostMapping("/{quizNo}")
+        public ResponseEntity<?> writeTestcase(
+                @PathVariable Long quizNo, @RequestBody TestcaseDTO testcaseDTO) throws
+                MyException {
+            testcaseDTO.setQuizNo(quizNo);
+            try {
+                service.addTestcase(testcaseDTO);
+                String msg = "테스트케이스 추가 성공";
+                return new ResponseEntity<>(msg, HttpStatus.OK);
+            } catch (MyException e) {
+                throw new MyException(ErrorCode.TESTCASE_NOT_SAVED);
+            }
+        }
 
     /* 테스트케이스 수정하기 : 관리자 */
     //    @PutMapping("/{testcaseNo}")
--- src/main/java/com/trianglechoke/codesparring/quiz/service/TestcaseService.java
+++ src/main/java/com/trianglechoke/codesparring/quiz/service/TestcaseService.java
@@ -1,43 +1,45 @@
 package com.trianglechoke.codesparring.quiz.service;
 
+import com.trianglechoke.codesparring.exception.MyException;
+import com.trianglechoke.codesparring.quiz.Repository.TestcaseInputRepository;
 import com.trianglechoke.codesparring.quiz.Repository.TestcaseRepository;
 
+import com.trianglechoke.codesparring.quiz.dto.TestcaseDTO;
+import com.trianglechoke.codesparring.quiz.dto.TestcaseInputDTO;
+import com.trianglechoke.codesparring.quiz.entity.Testcase;
+import com.trianglechoke.codesparring.quiz.entity.TestcaseInput;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class TestcaseService {
-    @Autowired private TestcaseRepository repository;
+import java.util.ArrayList;
+import java.util.List;
 
-    /* quizNo에 해당하는 testcase 목록 조회 */
-    //    public List<TestcaseDTO> findAllByQuizNo(Long quizNo) throws FindException {
-    //        List<TestcaseDTO> tcDTOList = new ArrayList<>();
-    //        List<Object[]> list = repository.findAllByQuizNo(quizNo);
-    //        for (Object[] objArr : list) {
-    //            try {
-    //                TestcaseDTO dto =
-    //                        TestcaseDTO.builder()
-    //                                .testcaseNo(Long.valueOf(String.valueOf(objArr[0])))
-    //                                .quizNo(Long.valueOf(String.valueOf(objArr[1])))
-    //                                .build();
-    //
-    //                if (objArr[3] != null) dto.setTestcaseOutput(String.valueOf(objArr[3]));
-    //                tcDTOList.add(dto);
-    //            } catch (Exception e) {
-    //            }
-    //        }
-    //        return tcDTOList;
-    //    }
+@Service
+@Slf4j
+public class TestcaseService {
+    @Autowired private TestcaseRepository testcaseRepository;
+    @Autowired private TestcaseInputRepository inputRepository;
 
     /* testcase 추가 */
-    //    public void addTestcase(TestcaseDTO tcDTO) throws AddException {
-    //        Testcase tcEntity =
-    //                Testcase.builder()
-    //                        .quizNo(tcDTO.getQuizNo())
-    //                        .testcaseOutput(tcDTO.getTestcaseOutput())
-    //                        .build();
-    //        repository.save(tcEntity);
-    //    }
+    public void addTestcase(TestcaseDTO tcDTO) throws MyException {
+        Testcase tcEntity =
+                Testcase.builder()
+                        .quizNo(tcDTO.getQuizNo())
+                        .testcaseOutput(tcDTO.getTestcaseOutput())
+                        .build();
+        testcaseRepository.save(tcEntity);
+        log.error("{}",tcDTO.getTestcaseInputDTOList().size());
+        for(TestcaseInputDTO input:tcDTO.getTestcaseInputDTOList()) {
+            TestcaseInput inputEntity =
+                    TestcaseInput.builder()
+                            .testcaseNo(tcEntity.getTestcaseNo())
+                            .inputVar(input.getInputVar())
+                            .testcaseInput(input.getTestcaseInput())
+                            .build();
+            inputRepository.save(inputEntity);
+        }
+    }
 
     /* testcase 수정 */
     //    public void modifyTestcase(TestcaseDTO tcDTO) throws ModifyException {
