12827aade69145d446ba75ea371f338508d79a91
leejaeseong11 <easeon78@gmail.com>
2023-11-27 11:55:32 +0900

    feat: room member key

---
+++ src/main/java/com/trianglechoke/codesparring/room/dao/RoomMemberRepository.java
@@ -0,0 +1,8 @@
+package com.trianglechoke.codesparring.room.dao;
+
+import com.trianglechoke.codesparring.room.entity.RoomMember;
+import com.trianglechoke.codesparring.room.entity.RoomMemberKey;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomMemberRepository extends JpaRepository<RoomMember, RoomMemberKey> {}
