1e524c2893e2e10994774a945b7a233b380346ab
qbobl5 <ddddd991125@gmail.com>
2023-11-28 11:35:10 +0900

    fix repository

--- src/main/java/com/trianglechoke/codesparring/rankgame/repository/RankGameRepository.java
+++ src/main/java/com/trianglechoke/codesparring/rankgame/repository/RankGameRepository.java
@@ -26,7 +26,7 @@ public interface RankGameRepository extends JpaRepository<RankGame, Long> {
                             + "FROM rank_game r\n"
                             + "JOIN \"MEMBER\" m1 ON r.member1_no=m1.member_no\n"
                             + "JOIN \"MEMBER\" m2 ON r.member2_no=m2.member_no\n"
-                            + "WHERE member1_no=:memberNo OR member2_no=:memberNo",
+                            + "WHERE member1_no=:memberNo OR member2_no=:memberNo ORDER BY rank_no",
             nativeQuery = true)
     public List<Object[]> findListByMemberNo(Long memberNo);
 }
