065d662074cdae68a2dfaa96e4a096c53719e418
qbobl5 <ddddd991125@gmail.com>
2023-11-28 10:39:14 +0900

    fix rankgame exception
    예외처리 back에서 해주기

--- src/main/java/com/trianglechoke/codesparring/rankgame/service/RankGameService.java
+++ src/main/java/com/trianglechoke/codesparring/rankgame/service/RankGameService.java
@@ -1,5 +1,6 @@
 package com.trianglechoke.codesparring.rankgame.service;
 
+import com.trianglechoke.codesparring.exception.ErrorCode;
 import com.trianglechoke.codesparring.exception.MyException;
 import com.trianglechoke.codesparring.rankgame.dto.RankGameDTO;
 import com.trianglechoke.codesparring.rankgame.entity.RankGame;
@@ -15,14 +16,22 @@ public class RankGameService {
 
     /* save : 랭크게임 정보 저장 */
     public void addRankGame(RankGameDTO rankGameDTO) throws MyException {
+        if (rankGameDTO.getMember1No() == rankGameDTO.getMember2No()) {
+            throw new MyException(ErrorCode.RANK_NOT_SAVED);
+        }
         repository.saveRankGame(rankGameDTO.getMember1No(), rankGameDTO.getMember2No());
     }
 
     /* modify : 랭크게임 결과 업데이트 */
     public void modifyGameResult(RankGameDTO rankGameDTO) throws MyException {
+        Integer gr=rankGameDTO.getGameResult();
+        if(gr!=1 && gr!=2 && gr!=0) {
+            throw new MyException(ErrorCode.RANK_GAME_NOT_MODIFIED);
+        }
         Optional<RankGame> optRG=repository.findById(rankGameDTO.getRankNo());
         RankGame rankGameEntity=optRG.get();
         rankGameEntity.modifyGameResult(rankGameDTO.getGameResult());
         repository.save(rankGameEntity);
     }
+
 }
