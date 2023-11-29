bd6d569bdc5139e8077d613f4ab20af11f376efe
leejaeseong11 <easeon78@gmail.com>
2023-11-29 09:32:18 +0900

    feat: add room member service interface

--- src/main/java/com/trianglechoke/codesparring/room/service/RoomMemberService.java
+++ src/main/java/com/trianglechoke/codesparring/room/service/RoomMemberService.java
@@ -1,3 +1,11 @@
 package com.trianglechoke.codesparring.room.service;
 
-public interface RoomMemberService {}
+import com.trianglechoke.codesparring.room.dto.RoomMemberDTO;
+
+public interface RoomMemberService {
+    /* 대기방에 회원 추가 */
+    void addRoomMember(RoomMemberDTO roomMemberDTO);
+
+    /* 대기방에 회원 삭제 */
+    void removeMember(RoomMemberDTO roomMemberDTO);
+}
--- src/main/java/com/trianglechoke/codesparring/room/service/RoomMemberServiceImpl.java
+++ src/main/java/com/trianglechoke/codesparring/room/service/RoomMemberServiceImpl.java
@@ -2,10 +2,9 @@ package com.trianglechoke.codesparring.room.service;
 
 import com.trianglechoke.codesparring.room.dto.RoomMemberDTO;
 
-public class RoomMemberServiceImpl {
-    /* 대기방에 회원 추가 */
+public class RoomMemberServiceImpl implements RoomMemberService {
+
     public void addRoomMember(RoomMemberDTO roomMemberDTO) {}
 
-    /* 대기방에 회원 삭제 */
     public void removeMember(RoomMemberDTO roomMemberDTO) {}
 }
