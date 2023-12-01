ac26333bd42450cb9243cdd08670565305d300e8
suemini <121214637+suemini@users.noreply.github.com>
2023-12-01 15:14:24 +0900

    [Fix] import 문 수정

--- src/test/java/com/trianglechoke/codesparring/room/RoomMemberServiceTest.java
+++ src/test/java/com/trianglechoke/codesparring/room/RoomMemberServiceTest.java
@@ -7,7 +7,7 @@ import static org.mockito.Mockito.*;
 
 import com.trianglechoke.codesparring.exception.MyException;
 import com.trianglechoke.codesparring.member.entity.Member;
-import com.trianglechoke.codesparring.member.repository.MemberRepository;
+import com.trianglechoke.codesparring.member.dao.MemberRepository;
 import com.trianglechoke.codesparring.room.dao.RoomMemberRepository;
 import com.trianglechoke.codesparring.room.dao.RoomRepository;
 import com.trianglechoke.codesparring.room.dto.RoomMemberDTO;
