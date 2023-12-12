50f0f84a595204aea0085abb87d051ef47540bc3
KANU <65529758+KANU896@users.noreply.github.com>
2023-12-12 15:13:34 +0900

    feat: quizNo에 해당하는 TestCase가져오기

--- src/main/java/com/trianglechoke/codesparring/code/control/CodeSubmissionController.java
+++ src/main/java/com/trianglechoke/codesparring/code/control/CodeSubmissionController.java
@@ -8,15 +8,13 @@ import com.trianglechoke.codesparring.code.service.CodeService;
 import com.trianglechoke.codesparring.exception.ErrorCode;
 import com.trianglechoke.codesparring.exception.MyException;
 
+import com.trianglechoke.codesparring.quiz.dto.TestcaseDTO;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
@@ -254,4 +252,11 @@ public class CodeSubmissionController {
         // -------------------------------실행 끝-------------------------------
 
     }
+
+    @GetMapping("/{quizNo}")
+    public ResponseEntity<?> testcase(@PathVariable Long quizNo) {
+        List<CodeTestcaseDTO> list = service.findByQuizNo(String.valueOf(quizNo));
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
 }
