fd186e3731e02781baf2d274fd65559a9df59dec
qbobl5 <ddddd991125@gmail.com>
2023-12-14 09:22:43 +0900

    fix-rankgame: reportDate jsonFormat 수정

--- src/main/java/com/trianglechoke/codesparring/report/dto/ReportDTO.java
+++ src/main/java/com/trianglechoke/codesparring/report/dto/ReportDTO.java
@@ -1,5 +1,6 @@
 package com.trianglechoke.codesparring.report.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import java.util.Date;
@@ -15,6 +16,7 @@ public class ReportDTO {
     private String memberName;
     private Long quizNo;
     private String reportContent;
+    @JsonFormat(pattern="yy-MM-dd", timezone="Asia/Seoul")
     private Date reportDate;
     private String reportComment;
     private Integer reportType;
