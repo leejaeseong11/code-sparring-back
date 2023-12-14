2ba498531ce315b5fc5171e8a799f44e5ed75da5
suemini <121214637+suemini@users.noreply.github.com>
2023-12-14 19:49:44 +0900

    Feat : QuizAdmin 권한 부여

--- src/main/java/com/trianglechoke/codesparring/quiz/control/QuizAdminController.java
+++ src/main/java/com/trianglechoke/codesparring/quiz/control/QuizAdminController.java
@@ -11,9 +11,11 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 @RequestMapping("/admin/quiz")
 public class QuizAdminController {
     @Autowired private QuizServiceImpl service;
