2da714b1b96d8fc65fd6069cd9bfe5ff71c4d8b9
leejaeseong11 <easeon78@gmail.com>
2023-12-20 17:48:41 +0900

    fix: room update

--- src/main/java/com/trianglechoke/codesparring/room/dao/RoomRepository.java
+++ src/main/java/com/trianglechoke/codesparring/room/dao/RoomRepository.java
@@ -2,9 +2,12 @@ package com.trianglechoke.codesparring.room.dao;
 
 import com.trianglechoke.codesparring.room.entity.Room;
 
+import jakarta.persistence.LockModeType;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -26,5 +29,6 @@ public interface RoomRepository extends JpaRepository<Room, Long> {
             nativeQuery = true)
     void updateRoom(@Param("roomNo") Long roomNo);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     void deleteById(Long id);
 }
